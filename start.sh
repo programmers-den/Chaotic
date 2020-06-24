3fbad6f3db06be7047d6df8c080df56c6c442e36
Proxy <proxneon960@gmail.com>
2020-06-24 07:14:54 +0100

    Nade start.sh loop anf pull from git

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
+while :
+do
+  pkill -f chaotic-js
+  if [ -f .env ]
+  then
+    export $(cat .env | sed 's/#.*//g' | xargs)
+  fi
 
-cd js
-./node_modules/coffeescript/bin/coffee index.coffee
+  git pull
+  cd js
+  bash -c "exec -a chaotic-js ./node_modules/coffeescript/bin/coffee index.coffee"
+  sleep 3600
+done
