ee804b646049f4c598fece81169b862f92bfd14c
Proxy <proxneon960@gmail.com>
2020-06-24 07:53:34 +0100

    Improvements and structure (not tested)

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 while :
 do
   pkill -f chaotic-js
+  pkill -f chaotic-py
   if [ -f .env ]
   then
     export $(cat .env | sed 's/#.*//g' | xargs)
@@ -10,5 +11,8 @@ do
   git pull
   cd js
   bash -c "exec -a chaotic-js ./node_modules/coffeescript/bin/coffee index.coffee"
+  cd ../py
+  bash -c "exec -a chaotic-py python3 main.py"
+  cd ../
   sleep 3600
 done
