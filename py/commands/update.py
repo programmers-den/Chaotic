f438c171fab20ecc05d243a14979127b7720a62a
DMNight6 <arnokong365@gmail.com>
2020-06-25 12:03:48 +0800

    Edit and fix update.py (Request edit by yeongjae)

--- py/commands/update.py
+++ py/commands/update.py
@@ -1,7 +1,7 @@
 @bot.command()
 async def update(ctx):
-    data = ctx.guild.get_role(725512500220657674)
-    if ctx.author.roles in data:
+    role = ctx.guild.get_role(725512500220657674)
+    if role in ctx.author.roles:
         await ctx.send("Request Received, Rebooting in 10 second")
         await asyncio.sleep(10)
         try:
