9e914635533dbf99e5475d4c8b27d934ad88b68d
DMNight6 <arnokong365@gmail.com>
2020-06-28 17:03:27 +0800

    Add websocket

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
 
 class cppclient : public SleepyDiscord::DiscordClient {
 public:
@@ -19,8 +21,22 @@ public:
 };
 
 const char* TOKEN = std::getenv("TOKEN");
+typedef websocketpp::client<websocketpp::config::asio_client> client;
 
 int main() {
-	cppclient client(TOKEN, 2);
-	client.run();
+	cppclient bot(TOKEN, 2);
+	bot.run();
+
+	client c;
+
+	std::string uri = "ws://127.0.0.1:9010";
+	c.init_asio();
+	websocketpp::lib::error_code ec;
+	client::connection_ptr con = c.get_connection(uri, ec);
+	if (ec) {
+		std::cout << "Failed to connect to the websocket" << std::endl;
+	}
+
+	c.connect(con);
+	c.run();
 };
