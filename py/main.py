729a4a46bb0856cf8ca8735792f9511d6a268d34
pichu0357 <nachinarchess96@gmail.com>
2020-06-28 13:14:10 +0530

    modified py/main.py

--- py/main.py
+++ py/main.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 prefix = os.environ.get("PREFIX")
 token = os.environ.get("TOKEN")
 bot = commands.AutoShardedBot(command_prefix=prefix, case_insensitive=True)
-bot.remove_command("help")
 
 async def connect():
     while True:
@@ -13,36 +12,11 @@ async def connect():
             ws = await websockets.connect("ws://127.0.0.1:9010")
             print("PY: Connected to socket")
             break
-        except OSError:
-            pass
+        except OSError: pass
     while True:
         msg = json.loads(await ws.recv())
         if "py" in msg["recipients"]:
-            if msg["data"] == "cmdlist":
-                cmds = {}
-                for cmd in bot.commands:
-                    print("PY: "+str(bot.commands))
-                    cmds[cmd.name] = cmd.description
-                await ws.send(json.dumps({
-                    "recipients":[msg["sender"]],
-                    "sender":"py",
-                    "data":cmds,
-                    "id":msg["id"]
-                }))
-            elif msg["data"] == "eval":
-                await ws.send(json.dumps({
-                    "recipients":[msg["sender"]],
-                    "sender":"py",
-                    "data":eval(msg["data"]),
-                    "id":msg["id"]
-                }))
-            else:
-                await ws.send(json.dumps({
-                    "recipients":[msg["sender"]],
-                    "sender":"py",
-                    "data":"Not a valid command",
-                    "id":msg["id"]
-                }))
+            await ws.send(json.dumps({"recipients":msg["sender"], "sender":"py", "info":eval(msg["info"])}))
 
 @bot.listen()
 async def on_ready():
