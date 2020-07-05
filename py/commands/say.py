843768d47e890c2646ea2f23ce7b8042ae7dc5a0
DMNight6 <arnokong365@gmail.com>
2020-07-05 22:38:32 +0800

    Update say.py

--- py/commands/say.py
+++ py/commands/say.py
@@ -1,6 +1,5 @@
 @bot.command()
 async def say(ctx, *, args=None):
     await ctx.message.delete()
-    await ctx.message.delete()
     if args: await ctx.send(args)
     else: await ctx.send("Please supply something to say!")
