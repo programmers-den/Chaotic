1c3aafd96832fd482e877e7804a09c2a8c97f98d
pichu0357 <nachinarchess96@gmail.com>
2020-07-24 17:08:10 +0530

    await ctx.message.delete() added in info.py

---
+++ py/commands/info.py
@@ -0,0 +1,9 @@
+@bot.command()
+async def info(ctx):
+    await ctx.message.delete()
+    embed = discord.Embed(title="Chaotic bot info", color=discord.Color.from_rgb(34,139,34))
+    embed.add_field(name="Founder", value="Technisha Circuit")
+    embed.add_field(name="Version", value="0.1.0")
+    embed.add_field(name="Release Date", value="06/24/2020")
+    embed.add_field(name="Github", value="https://github.com/Programmer-s-Organization/Chaotic")
+    await ctx.send(embed=embed)
