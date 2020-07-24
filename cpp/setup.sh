2a45304608b7bbd11b374ba760fd735b66bdd7a0
DMNight6 <arnokong365@gmail.com>
2020-07-24 21:23:52 +0800

    Create setup.sh

---
+++ cpp/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bash -c "sudo apt install libcurl-dev openssl-dev"
+bash -c "cd lib"
+if [[ -f sleepy-discord ]]
+then
+    bash -c "rm -rf sleepy-discord"
+    bash -c "git clone https://github.com/yourWaifu/sleepy-discord.git"
+fi
+
+bash -c "cd .."
+bash -c "cmake"
