443249292e7a772d67e6cbfff0f56234cff8cf16
pichu0357 <nachinarchess96@gmail.com>
2020-07-24 16:55:37 +0530

    project.py is modified

--- py/commands/project.py
+++ py/commands/project.py
@@ -1,6 +1,7 @@
 @bot.command()
 async def info(ctx):
-    embed = discord.Embed(title="Chaotic bot info", color=discord.Color.from_rgb(255,0,255))
+    await ctx.message.delete()
+    embed = discord.Embed(title="Chaotic bot info", color=discord.Color.from_rgb(34,139,34))
     embed.add_field(name="Founder", value="Technisha Circuit")
     embed.add_field(name="Version", value="0.1.0")
     embed.add_field(name="Release Date", value="06/24/2020")
